b2738dd6e9e41d2b408144435f2a3174cf8aea3c
Nesrin D. Tanyeri <daghantanyeri@gmail.com>
2024-11-11 16:23:46 +0100

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ import Home from "./components/Home";
 import ProductList from "./ProductPage/ProductList";
 import CartPage from "./pages/CartPage";
 import SignInPage from './pages/SignInPage';
-import SignUpPage from "./pages/SignUpPage"; // Corrected import
+
 
 function App() {
   const router = createBrowserRouter(
@@ -19,7 +19,6 @@ function App() {
         <Route index element={<Home />} />
         <Route path="/products" element={<ProductList />} />
         <Route path="/cart" element={<CartPage />} />
-        <Route path="/signup" element={<SignUpPage />} /> 
         <Route path="/signin" element={<SignInPage />} /> 
       </Route>
     )
