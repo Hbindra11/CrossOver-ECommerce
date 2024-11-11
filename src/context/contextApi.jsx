3c1d6d2799990dd45c6af9d65c4e74f98a20c48e
krishna <krisch.webdev@gmail.com>
2024-11-11 12:52:10 +0100

    productspage

--- src/context/contextApi.jsx
+++ src/context/contextApi.jsx
@@ -1,53 +1,53 @@
-import {createContext, useReducer} from 'react';
+import { createContext, useReducer } from "react";
 
-const initailState = {
-    products: [],
-    cart: [],
-    cartSubTotal: 0,
-    cartTax: 0,
-    cartTotal: 0,
+const initialState = {
+  products: [],
+  cart: [],
+  cartSubTotal: 0,
+  cartTax: 0,
+  cartTotal: 0,
 };
 
 const storeReducer = (state, action) => {
-    switch (action.type) {
-        case "products":
-            return {
-                ...state,
-                products: action.payload,
-            };
-        case "cart":
-            return {
-                ...state,
-                cart: action.payload,
-            };
-        case "cartSubTotal":
-            return {
-                ...state,
-                cartSubTotal: action.payload,
-            };
-        case "cartTax":
-            return {
-                ...state,
-                cartTax: action.payload,
-            };
-        case "cartTotal":
-            return {
-                ...state,
-                cartTotal: action.payload,
-            };
-        default:
-            return state;
-    }
+  switch (action.type) {
+    case "products":
+      return {
+        ...state,
+        products: action.payload,
+      };
+    case "cart":
+      return {
+        ...state,
+        cart: action.payload,
+      };
+    case "cartSubTotal":
+      return {
+        ...state,
+        cartSubTotal: action.payload,
+      };
+    case "cartTax":
+      return {
+        ...state,
+        cartTax: action.payload,
+      };
+    case "cartTotal":
+      return {
+        ...state,
+        cartTotal: action.payload,
+      };
+    default:
+      return state;
+  }
 };
 
 export const StoreContext = createContext();
 
-const StoreProvider = ({children}) => {
-    const [state, dispatch] = useReducer(storeReducer, initailState);
+export const StoreProvider = ({ children }) => {
+  const [state, dispatch] = useReducer(storeReducer, initialState);
 
-    return (
-        <StoreContext.Provider value={{state, dispatch}}>
-            {children}
-        </StoreContext.Provider>
-    );
-}
+  return (
+    <StoreContext.Provider value={{ state, dispatch }}>
+      {children}
+    </StoreContext.Provider>
+  );
+};
