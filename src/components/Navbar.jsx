eb853986a5a9a06cc0f81ba336bf5289af20ebd1
Hema Bindra <hbindra11@yahoo.com>
2024-11-11 11:42:51 +0100

    fixed navbar to show temporary logo and Home and shoppingcart link

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,24 +2,52 @@ import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
-    <div className="navbar bg-base-100">
-     
-        <nav className=" p-4">
-          <div className="container mx-auto flex justify-between items-center">
-            {/* The Link component takes care of updating the history in the browser */}
-            <Link to="/" className="text-white text-lg font-bold no-underline">
-              Fake Store
-            </Link>
-            <div className="flex space-x-4">
-              <Link to="/" className="text-gray-300 hover:text-white">
-                Home
-              </Link>
+    <nav className="bg-base-100 p-4 ">
+      <div className="container mx-auto flex  items-center">
+        {/* The Link component takes care of updating the history in the browser */}
+        {/* <Link to="/" className="text-black text-lg font-bold no-underline"> */}
+          FakeStore
+        {/* </Link> */}
+        <div className="container mx-auto flex justify-end items-cente flex space-x-4">
+          <Link to="/" className="text-black hover:text-gray-200">
+            Home
+          </Link>
+        </div>
+        <div className="dropdown dropdown-end">
+          <div tabIndex={0} role="button" className="btn btn-ghost btn-circle">
+            <div className="indicator">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-5 w-5"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"
+                />
+              </svg>
+              <span className="badge badge-sm indicator-item">8</span>
             </div>
           </div>
-          
-        </nav>
+          <div
+            tabIndex={0}
+            className="card card-compact dropdown-content bg-base-100 z-[1] mt-3 w-52 shadow"
+          >
+            <div className="card-body">
+              <span className="text-lg font-bold">8 Items</span>
+              <span className="text-info">Subtotal: $999</span>
+              <div className="card-actions">
+                <button className="btn btn-primary btn-block">View cart</button>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
-   
+    </nav>
   );
 };
 export default Navbar;
